03fa9b8891ce1e64423d5e265e8c67811a9c1372
Joel Lacey <joellaceybusiness@gmail.com>
2020-06-08 11:25:48 -0600

    Add basic controlled form color input

--- src/containers/ColorForm/ColorForm.js
+++ src/containers/ColorForm/ColorForm.js
@@ -1,16 +1,27 @@
 import React, { Component } from 'react';
 
-class ColorForm extends Component {
+export default class ColorForm extends Component {
     constructor() {
         super()
         this.state = { color: '' }
     }
 
+    handleChange = e => {
+        this.setState({ [e.target.name]: e.target.value })
+    }
+
     render() {
         return (
-            <input></input>
+            <form>
+                <h3>Please enter your starting color</h3>
+                <input
+                    type='text'
+                    placeholder='color'
+                    value={this.state.title}
+                    name='color'
+                    onChange={this.handleChange}
+                />
+            </form>
         )
     }
 }
-
-export default ColorForm;
