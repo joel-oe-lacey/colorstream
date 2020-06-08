dbbe2781df23cb897370b91d45f58f7c510a6a5e
Joel Lacey <joellaceybusiness@gmail.com>
2020-06-08 11:20:33 -0600

    Initial Commit

---
+++ src/components/ColorBox/ColorBox.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const ColorBox = ({ props: {hex}}) => {
+    return (
+        <section></section>
+    )
+}
+
+export default ColorBox;
---
+++ src/containers/ColorForm/ColorForm.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class ColorForm extends Component {
+    constructor() {
+        super()
+        this.state = { color: '' }
+    }
+
+    render() {
+        return (
+            <input></input>
+        )
+    }
+}
+
+export default ColorForm;
