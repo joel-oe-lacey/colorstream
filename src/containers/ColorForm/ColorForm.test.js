ec53c114a0d9d3f26f6d4b8ceb54d9e9cf3bc39e
Joel Lacey <joellaceybusiness@gmail.com>
2020-06-08 14:09:52 -0600

    Update test snapshots

--- src/containers/ColorForm/ColorForm.test.js
+++ src/containers/ColorForm/ColorForm.test.js
@@ -12,4 +12,18 @@ describe('ColorForm', () => {
     it('should match the snapshot', () => {
         expect(wrapper).toMatchSnapshot();
     })
+
+    it('should update the state in state when handle change is called', () => {
+        const mockEventColor = {
+            target: {
+                name: 'color',
+                value: 'a3b1c699'
+            }
+        }
+        const expectedColor = 'a3b1c699';
+
+        wrapper.instance().handleChange(mockEventColor);
+
+        expect(wrapper.state('color')).toEqual(expectedColor);
+    })
 })
