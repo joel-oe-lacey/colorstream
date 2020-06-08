36ebd5229fcf1b98ec66c8f4a71bbedb48bef8d9
Joel Lacey <joellaceybusiness@gmail.com>
2020-06-08 12:55:24 -0600

    Add color generation and initial state

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -2,18 +2,27 @@ import React, { Component } from 'react';
 import './App.scss';
 import ColorForm from '../ColorForm/ColorForm';
 import ColorCont from '../../components/ColorCont/ColorCont';
+const tinycolor = require("tinycolor2");
 
 export default class App extends Component {
   constructor() {
     super()
-    this.state = { color: '' }
+    this.state = { colors: ['#E0E5EC', '#E0E5EC', '#E0E5EC', '#E0E5EC'] }
+  }
+
+  selectColor = color => {
+    //add normalization for a variety of color inputs
+    const selectedColor = '#' + color;
+    const colorParse = tinycolor(selectedColor).tetrad();
+    const generatedColors = colorParse.map(function (t) { return t.toHexString(); });
+    this.setState({ colors: [...generatedColors]});
   }
 
   render() {
     return (
       <section className="App">
-        <ColorForm />
-        <ColorCont />
+        <ColorForm selectColor={this.selectColor}/>
+        <ColorCont colors={this.state.colors}/>
       </section>
     )
   };
