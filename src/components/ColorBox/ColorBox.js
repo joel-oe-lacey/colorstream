8b7f5d34444b05b0e5c0a98835b0f211db108916
Joel Lacey <joellaceybusiness@gmail.com>
2020-06-08 14:03:33 -0600

    Add ColorBox hex display

--- src/components/ColorBox/ColorBox.js
+++ src/components/ColorBox/ColorBox.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import "./ColorBox.scss";
+const tinycolor = require("tinycolor2");
 
 const ColorBox = ({color}) => {
+    const greyscale = tinycolor(color).greyscale().toString();
+    
     return (
         <section className='color-cont'>
             <div style={{ backgroundColor: `${color}` }}
                 className='color-cont-inner'
-            ></div>
+            >
+            <h3 style={{ color: `${greyscale}` }}
+              >{`${color}`}</h3>  
+            </div>
         </section>
     )
 }
