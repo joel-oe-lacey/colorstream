475c07a60927206f3833fadfd0127fd9831b6358
Joel Lacey <joellaceybusiness@gmail.com>
2020-06-08 12:56:17 -0600

    Add key to mapped ColorBox generation

--- src/components/ColorCont/ColorCont.js
+++ src/components/ColorCont/ColorCont.js
@@ -3,8 +3,8 @@ import "./ColorCont.scss";
 import ColorBox from '../ColorBox/ColorBox';
 
 const ColorCont = ({colors}) => {
-    const colorDisplay = colors.map(color => {
-        return <ColorBox color={color} />
+    const colorDisplay = colors.map((color, index) => {
+        return <ColorBox key={index} color={color} />
     })
 
     return (
