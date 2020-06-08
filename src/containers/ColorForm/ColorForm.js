189d6e27ffd1dadef3e929fb419e107e4b33f32f
Joel Lacey <joellaceybusiness@gmail.com>
2020-06-08 14:04:51 -0600

    Add color selection awareness to ColorForm

--- src/containers/ColorForm/ColorForm.js
+++ src/containers/ColorForm/ColorForm.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './ColorForm.scss';
+const tinycolor = require("tinycolor2");
 
 export default class ColorForm extends Component {
     constructor() {
@@ -8,32 +9,54 @@ export default class ColorForm extends Component {
     }
 
     handleChange = e => {
-        this.setState({ [e.target.name]: e.target.value })
+        this.setState({ [e.target.name]: e.target.value });
     }
 
     submitColor = e => {
         e.preventDefault();
         const { selectColor } = this.props;
         selectColor(this.state.color);
-        this.resetInputs();
+        this.resetInputs(e);
     }
 
-    resetInputs = () => {
-        this.setState({color: ''})
+    resetInputs = e => {
+        this.setState({color: ''});
+        document.getElementById("colorForm").reset()
+    }
+
+    displaySelection = e => {
+        const colorEntered = `#${this.state.color}`
+        const colorCheck = tinycolor(colorEntered).isValid();
+        if (colorCheck) {
+            const greyscale = tinycolor(colorEntered).greyscale().toString();
+            e.target.style.background = colorEntered;
+            e.target.style.color = greyscale;
+        }
+    }
+
+    resetButtonBackground = e => {
+        e.target.style.background = '#E0E5EC';
+        e.target.style.color = '#a3b1c699';   
     }
 
     render() {
         return (
-            <form className='colorForm'>
-                <h1>Please Enter A Color</h1>
+            <form id='colorForm'>
+                <h1 className='color-change-5x'>Color Stream</h1>
                 <input
                     type='text'
-                    placeholder='color'
+                    placeholder='Please Enter A Color'
                     value={this.state.title}
                     name='color'
                     onChange={this.handleChange}
                 />
-                <button onClick={this.submitColor}>Submit</button>
+                <div className='outer-button'>
+                    <button 
+                        onClick={this.submitColor} 
+                        onMouseOver={this.displaySelection}
+                        onMouseOut={this.resetButtonBackground}
+                    >Submit</button>
+                </div>
             </form>
         )
     }
